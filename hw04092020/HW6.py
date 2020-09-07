6cf7a86dbca3e11fa769cd0c33130576ee0dc78e
vagaganova <69433218+vagaganova@users.noreply.github.com>
2020-09-07 19:25:51 +0300

    Update HW6.py
    Uncommented

--- hw04092020/HW6.py
+++ hw04092020/HW6.py
@@ -1,12 +1,12 @@
 from itertools import count, cycle
-# for i in count(14):
-#     if i > 30:
-#         break
-#     print(i)
+ for i in count(14):
+     if i > 30:
+         break
+     print(i)
 
-# el = 0
-# for letter in cycle(['hello']):
-#     if el > 20:
-#         break
-#     print(letter)
-#     el += 1
+ el = 0
+ for letter in cycle(['hello']):
+     if el > 20:
+         break
+     print(letter)
+     el += 1
